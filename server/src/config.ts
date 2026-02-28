8a6f2b70cabd07b8a5810016e11a9d148644e793
Ethan Larrar <ethanlarrar@gmail.com>
2026-02-28 15:12:10 +0200

    fix a lot

--- server/src/config.ts
+++ server/src/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const config = {
   PORT: parseInt(process.env.PORT || '3001', 10),
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/audit_logs',
+  PGQL_DB_URL: process.env.PGQL_DB_URL || 'postgres://postgres:postgres@localhost:5432/audit_logs',
   NODE_ENV: (process.env.NODE_ENV || 'development') as 'development' | 'production' | 'test',
   STS_URL: process.env.STS_URL || 'http://localhost:3000',
   SERVER_URL: process.env.SERVER_URL || 'http://localhost:3001',
